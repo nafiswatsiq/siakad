e43c2bd5d959ac3eafdcd93cbcbbe4e7a1356ddf
yeftaa-magangjogja <yeftach.magangjogja@gmail.com>
2025-03-05 12:07:04 +0700

    Mahasiswa  05/03/2025

---
+++ database/migrations/2025_03_05_045341_create_mahasiswas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Kelas;
+use App\Models\Prodi;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Kelas::class)->constrained();
+            $table->foreignIdFor(Prodi::class)->constrained();
+            $table->string('jenis_kelamin');
+            $table->string('nim');
+            $table->integer('semester');
+            $table->date('tanggal_lahir');
+            $table->string('alamat');
+            $table->string('no_tlp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
