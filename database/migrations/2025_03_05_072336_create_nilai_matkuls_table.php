eed04b582a01cc5807784db5104b6f5235855766
YeftaCharrand <yeftakusuma123@gmail.com>
2025-03-05 15:01:21 +0700

    revisi migration nilai_matkul

--- database/migrations/2025_03_05_072336_create_nilai_matkuls_table.php
+++ database/migrations/2025_03_05_072336_create_nilai_matkuls_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('nilai_matkuls', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->foreignIdFor(Mahasiswa::class)->constrained();
             $table->foreignIdFor(Matkul::class)->constrained();
             $table->decimal('nilai', 5, 2);
