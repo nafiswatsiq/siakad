c1134a802efd95dc1fc99d0a89ff0541d651c151
YeftaCharrand <yeftakusuma123@gmail.com>
2025-05-20 16:19:02 +0700

    impr:Manambahkan Seeder data Umum

---
+++ database/seeders/TahunAjaranSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class TahunAjaranSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $now = Carbon::now();
+
+        DB::table('tahun_ajarans')->insert([
+            [
+                'nama' => '2023/2024',
+                'aktif' => false,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nama' => '2023/2024',
+                'aktif' => false,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nama' => '2024/2025',
+                'aktif' => true, // aktif tahun ajaran sekarang
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nama' => '2024/2025',
+                'aktif' => true, // aktif tahun ajaran sekarang
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+        ]);
+    }
+}
