b56820c8d4ac543a2583a50af6bba4df17adabeb
YeftaCharrand <yeftakusuma123@gmail.com>
2025-05-22 09:47:48 +0700

    revisi nilai matkul

--- app/Models/NilaiMatkul.php
+++ app/Models/NilaiMatkul.php
@@ -4,27 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Nilai extends Model
+class NilaiMatkul extends Model
 {
     protected $fillable = [
         'mahasiswa_id',
-        'ips',
-        'ipk',
-        'semester_id',
-        'tahun_ajaran_id',
-        'status'
+        'matkul_id',
+        'nilai',
+        'semester_id'
     ];
 
     public function mahasiswa()
     {
         return $this->belongsTo(Mahasiswa::class, 'mahasiswa_id');
     }
+
+    public function matkul()
+    {
+        return $this->belongsTo(Matkul::class, 'matkul_id');
+    }
     public function semester()
     {
         return $this->belongsTo(Semester::class, 'semester_id');
-    }
-    public function tahun_ajaran()
-    {
-        return $this->belongsTo(TahunAjaran::class, 'tahun_ajaran_id');
     }
 }
